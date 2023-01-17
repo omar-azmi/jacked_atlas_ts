7b07dac07c9cee3b5067f43daf78d1e4772de5d4
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2023-01-17 03:09:14 +0300

    store mask image data more memory efficiently by using `Blob` instead of `ImageData`

--- src/mod.ts
+++ src/mod.ts
@@ -20,6 +20,8 @@ interface JAtlas {
 	entries: { [id: number]: JAtlasEntry }
 }
 
+const isBase64Image = (obj?: string): obj is Base64Image => obj === undefined ? false : obj.startsWith("data:image/")
+
 const
 	clipmask_offcanvas = new OffscreenCanvas(10, 10),
 	clipmask_offctx = clipmask_offcanvas.getContext("2d", { willReadFrequently: true }) as OffscreenCanvasRenderingContext2D
@@ -29,11 +31,11 @@ clipmask_offctx.imageSmoothingEnabled = false
 class ClipMask {
 	rect: Rect
 	/** file path of the source image, or base64 data uri <br>
-	 * if a base64 data uri is provided, then once it has been turned trasnparent and converted into `this.data_uri`, `this.src_url` will be deleted
+	 * if a base64 data uri is provided, then once it has been turned trasnparent and converted into `this.data_uri`, `this.src_url` will be deleted <br>
+	 * at any given time, one of either `this.src_url` or `this.data_blob` will be present
 	*/
 	src_url?: FilePath | Base64Image
-	data_uri?: Base64Image
-	mask_imgdata?: ImageData
+	data_blob?: Blob
 
 	constructor(src_url?: string, rect?: Partial<Rect>) {
 		this.src_url = src_url
@@ -44,23 +46,23 @@ class ClipMask {
 	fromURL = (src_url: string, rect: Partial<Rect>) => {
 		const img = new Image()
 		this.src_url = src_url
-		return new Promise<ImageData>((resolve, reject) => {
+		return new Promise<void>((resolve, reject) => {
 			img.onerror = () => reject(`failed to load url:\n\t${src_url}`)
 			img.onload = () => {
-				rect.width = img.width
-				rect.height = img.height
-				clipmask_offcanvas.width = rect.width
-				clipmask_offcanvas.height = rect.height
+				const { width: w, height: h } = img
+				rect.width = w
+				rect.height = h
+				clipmask_offcanvas.width = w
+				clipmask_offcanvas.height = h
 				clipmask_offctx.globalCompositeOperation = "copy"
 				clipmask_offctx.drawImage(img, 0, 0)
-				const { data } = clipmask_offctx.getImageData(0, 0, rect.width, rect.height)
+				const { data } = clipmask_offctx.getImageData(0, 0, w, h)
 				resolve(
 					this.fromBuffer(data, rect as { x?: number, y?: number, width: number, height: number })
 				)
 			}
 			img.src = src_url
 		})
-
 	}
 
 	/** load an image from a string of data uri, such as: `"data:image/gif;base64,R0l..."` */
@@ -69,12 +71,15 @@ class ClipMask {
 	}
 
 	/** load an image from  a `Uint8Array` of RGBA pixels. the width and height must be defined in the passed `rect` */
-	fromBuffer = (buf: Uint8Array | Uint8ClampedArray, rect: { x?: number, y?: number, width: number, height: number }): ImageData => {
+	fromBuffer = async (buf: Uint8Array | Uint8ClampedArray, rect: { x?: number, y?: number, width: number, height: number }): Promise<void> => {
 		rect.x ??= 0
 		rect.y ??= 0
 		this.rect = rect as Rect
-		this.mask_imgdata = new ImageData(ClipMask.turnTransparent(buf) as Uint8ClampedArray, rect.width, rect.height)
-		return this.mask_imgdata
+		clipmask_offctx.globalCompositeOperation = "copy"
+		clipmask_offctx.putImageData(new ImageData(ClipMask.turnTransparent(buf) as Uint8ClampedArray, rect.width, rect.height), 0, 0)
+		this.data_blob = await clipmask_offcanvas.convertToBlob({ type: "image/png" })
+		if (isBase64Image(this.src_url)) this.src_url = undefined
+		return
 	}
 
 	/** turn the provided buffer of pixels to become transparent where a black pixel is present */
@@ -84,14 +89,16 @@ class ClipMask {
 		return buf
 	}
 
-	clearImageData = () => this.mask_imgdata = undefined
+	clearDataBlob = () => this.data_blob = undefined
 
 	clipImage = async (img: CanvasImageSource) => {
-		if (this.mask_imgdata === undefined) await this.fromURL(this.src_url!, this.rect)
-		clipmask_offctx.globalCompositeOperation = "copy"
-		clipmask_offctx.putImageData(this.mask_imgdata!, 0, 0)
-		clipmask_offctx.globalCompositeOperation = "source-in"
-		clipmask_offctx.drawImage(img, -this.rect.x, -this.rect.y)
+		if (this.data_blob === undefined) await this.fromURL(this.src_url!, this.rect)
+		return createImageBitmap(this.data_blob!).then(mask_img => {
+			clipmask_offctx.globalCompositeOperation = "copy"
+			clipmask_offctx.drawImage(mask_img, 0, 0)
+			clipmask_offctx.globalCompositeOperation = "source-in"
+			clipmask_offctx.drawImage(img, -this.rect.x, -this.rect.y)
+		})
 	}
 }
 
